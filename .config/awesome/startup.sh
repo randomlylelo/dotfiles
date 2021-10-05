43632de158c34eb6f0e82947b3d08ca53b35fedd
Leo <38267386+randomlylelo@users.noreply.github.com>
2021-10-05 15:12:36 -0700

    update startup script to have another type of monitor. and new vimnotes

--- .config/awesome/startup.sh
+++ .config/awesome/startup.sh
@@ -2,10 +2,30 @@
 
 # Check dual monitor setup
 # Check if HDMI is plugged in, if so then run dual monitor script.
+# if xrandr | grep -q "HDMI-0 connected"; then
+#     # RESOLUTION SETTINGS
+#     # This sets your HDMI Port Res
+#     xrandr --output HDMI-0 --mode 3840x2160 --rate 60
+#     # This sets your laptop monitor to its best resolution.
+#     xrandr --output eDP-1-0  --mode 1920x1080 --rate 144
+
+#     # MONITOR ORDER
+#     xrandr --output HDMI-0 --right-of eDP-1-0
+
+#     # PRIMARY MONITOR
+#     # This sets your laptop monitor as your primary monitor.
+#     xrandr --output eDP-1-0 --primary
+
+#     # Increase zoom
+#     # xrandr --output HDMI-0 --scale 1x1
+#     # xrandr --output eDP-1-0 --scale 1.2x1.2
+# fi
+
+# Dual monitor script for small screen.
 if xrandr | grep -q "HDMI-0 connected"; then
     # RESOLUTION SETTINGS
     # This sets your HDMI Port Res
-    xrandr --output HDMI-0 --mode 3840x2160 --rate 60
+    xrandr --output HDMI-0 --mode 1920x1080 --rate 60
     # This sets your laptop monitor to its best resolution.
     xrandr --output eDP-1-0  --mode 1920x1080 --rate 144
 
@@ -20,7 +40,6 @@ if xrandr | grep -q "HDMI-0 connected"; then
     # xrandr --output HDMI-0 --scale 1x1
     # xrandr --output eDP-1-0 --scale 1.2x1.2
 fi
-
 
 # Autorun function, checks if program is already running, if not start
 function run {
